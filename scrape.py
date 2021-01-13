3791a6ee988706a2c6fd4671dd9d60c06022357f
Elliot Wilens <wilensel@gmail.com>
2021-01-12 21:06:58 -0600

    Fix team_stats, start work on games info

--- scrape.py
+++ scrape.py
@@ -5,6 +5,41 @@ import pandas as pd
 import requests
 from bs4 import BeautifulSoup
 
+team_abbrevs = [
+    "crd",
+    "atl",
+    "rav",
+    "buf",
+    "car",
+    "chi",
+    "cin",
+    "cle",
+    "dal",
+    "den",
+    "det",
+    "gnb",
+    "htx",
+    "clt",
+    "jax",
+    "kan",
+    "mia",
+    "min",
+    "nwe",
+    "nor",
+    "nyg",
+    "nyj",
+    "rai",
+    "phi",
+    "pit",
+    "sdg",
+    "sfo",
+    "sea",
+    "ram",
+    "tam",
+    "oti",
+    "was",
+]
+
 
 def scrape_team_stats(start_year, end_year, save_to=False):
     """
@@ -19,41 +54,6 @@ def scrape_team_stats(start_year, end_year, save_to=False):
             - file will only be saved if save_to != False
     """
     nfl_team_stats = []  # will contain one dict for each row
-
-    team_abbrevs = [
-        "crd",
-        "atl",
-        "rav",
-        "buf",
-        "car",
-        "chi",
-        "cin",
-        "cle",
-        "dal",
-        "den",
-        "det",
-        "gnb",
-        "htx",
-        "clt",
-        "jax",
-        "kan",
-        "mia",
-        "min",
-        "nwe",
-        "nor",
-        "nyg",
-        "nyj",
-        "rai",
-        "phi",
-        "pit",
-        "sdg",
-        "sfo",
-        "sea",
-        "ram",
-        "tam",
-        "oti",
-        "was",
-    ]
 
     for team in team_abbrevs:
 
@@ -95,7 +95,7 @@ def scrape_team_stats(start_year, end_year, save_to=False):
                 if result != []:
                     stat, val = result[0]
                     try:
-                        val = int(val)
+                        val = float(val)
                     except:
                         pass
                     opp_stats[stat] = val
@@ -114,4 +114,64 @@ def scrape_team_stats(start_year, end_year, save_to=False):
         teams_df.to_pickle("data/" + str(save_to))
 
     return teams_df
+
+
+def scrape_games(start_year, end_year, save_to=False):
+    """
+        Builds a DataFrame containing all games from start_year to end_year (inclusive).
+    """
+
+    games = []
+
+    for team in team_abbrevs:
+
+        for year in range(start_year, end_year + 1):
+
+            url = f"https://www.pro-football-reference.com/teams/{team}/{year}.htm"
+
+            response = requests.get(url)
+            if response.status_code == 404:
+                continue
+
+            soup = BeautifulSoup(response.text, "lxml")
+            tables = soup.find_all("tbody")
+            sched_table = tables[1].find_all("tr")
+
+            for row in sched_table:
+                game = {}
+                game["team"] = team
+                game["year"] = year
+
+                try:
+                    opponent = re.search(r"href=\"/teams/([\w]{3}).*", str(row))[1]
+                    game["opp"] = opponent
+                except:
+                    pass
+                #     print(opponent)
+                results = re.findall(r"data-stat=\"([\w]+)\">([@\w-]+)", str(row))
+                for result in results:
+                    #         print(result)
+                    if result != []:
+                        stat, val = result
+                        try:
+                            game[stat] = float(val)
+                        except:
+                            game[stat] = val
+                if len(game) > 0:
+                    games.append(game)
+                else:
+                    continue
+
+    sched_df = pd.DataFrame(games)
+
+    if save_to:
+        sched_df.to_pickle("data/" + str(save_to))
+
+    return sched_df
+
+
+teams_df = scrape_team_stats(
+    start_year=1960, end_year=2020, save_to="team_stats_scraped.pickle"
+)
+sched_df = scrape_games(start_year=1960, end_year=2020, save_to="games_scraped.pickle")
 
